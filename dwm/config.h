8048eaa4861e0c35745e0f6f7623dc0921a28876
mrblue32 <lumi_isme@proton.me>
2023-12-12 11:37:55 -0300

    oke

--- dwm/config.h
+++ dwm/config.h
@@ -6,8 +6,8 @@ static const unsigned int gappx     = 12;        /* gaps size between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=9", "fontawesome:size=9" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "hack:size=9", "fontawesome:size=9" };
+static const char dmenufont[]       = "hack:size=10";
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
